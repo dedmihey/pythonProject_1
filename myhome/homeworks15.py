d452e3b83840f3fed80d722ef7f4c39be470f8b0
dedmihey <dedmihey@gmail.com>
2024-05-02 19:28:41 +0300

    Изменена структура кода

--- myhome/homeworks15.py
+++ myhome/homeworks15.py
@@ -2,9 +2,9 @@ class Houme:
     def __init__(self):
         self.numberOfFloors = 10
 
-    current_floors = 1
-    while current_floors <= 10:
-        print('Текущий этаж равен ', current_floors)
-        current_floors += 1
+current_floors = 1
+while current_floors <= 10:
+    print('Текущий этаж равен ', current_floors)
+    current_floors += 1
 home = Houme()
 
