7c7d26a0c31a6530ce19c5cfbc7dca7de3faf637
dedmihey <dedmihey@gmail.com>
2024-05-22 15:51:41 +0300

    Создан новый файл

--- myhome/homeworks25.py
+++ myhome/homeworks25.py
@@ -1,7 +1,9 @@
 file_name = 'read_.txt'
 with open(file_name, mode='r', encoding='utf8') as file:
     for line in file:
-        print(line)
+        print(line, end='')
+
+print(file.closed)
 
 
 
