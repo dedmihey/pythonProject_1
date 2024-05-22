a12d5fdfc1aebbc5ddc07e0376340e75b6d0d0e9
dedmihey <dedmihey@gmail.com>
2024-05-22 15:25:10 +0300

    Внесены изменения

--- myhome/homeworks24.py
+++ myhome/homeworks24.py
@@ -1,6 +1,8 @@
+from pprint import pprint
+
 file_name = 'read_.txt'
 print(file_name)
 file = open(file_name, mode='rb')
 file_content = file.read()
 file.close()
-print(file_content)
+pprint(file_content)
---
+++ myhome/homeworks25.py
@@ -0,0 +1,7 @@
+file_name = 'read_.txt'
+with open(file_name, mode='r', encoding='utf8') as file:
+    for line in file:
+        print(line)
+
+
+
